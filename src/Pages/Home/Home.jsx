7fb712ca0385f255b8167adf5e85d5d2511be4eb
antonio <antoniorc7@gmail.com>
2024-04-11 13:34:31 +0200

    fix: div for cards in superAdmin improved and cards in home shows from latest to oldest

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -166,7 +166,7 @@ export const Home = () => {
                                             </div>
                                         </div>
                                     )
-                                })}
+                                }).reverse()}
                         </div>
                     </>
 
