5f308ecaf10547e687d41f85edbcf7358d1ceeca
antonio <antoniorc7@gmail.com>
2024-04-14 14:05:54 +0200

    fix: logo added to header

--- src/common/Header/Header.jsx
+++ src/common/Header/Header.jsx
@@ -3,6 +3,7 @@ import './Header.css'
 import { Navigator } from '../Navigator/Navigator'
 import { useSelector, useDispatch } from "react-redux";
 import { userData, logout } from "../../app/slices/userSlice";
+import { IconNavigator } from '../IconNavigator/IconNavigator';
 
 
 export const Header = () => {
@@ -15,18 +16,18 @@ export const Header = () => {
         <div className="headerDesign">
             <div className='titleRow'>
             <div className='webTitle'>
-                POST IT!
-            </div>
-            </div>
-            <Navigator
+                <IconNavigator
                 path="/"
-                title="Home"
-            />
+                title={<img src='img/iconPNG.png' alt="Logo" />}
+                />
+            </div>
+            </div>
+            
             {reduxUser.tokenData.token ? (
                 reduxUser.tokenData.user.role === 'super_admin' ? (
                     <div className='navigatorDesign'>
                         <Navigator
-                            path='/superAdmin' title='ADMIN' />
+                            path='/superAdmin' title='admin area' />
                         <Navigator
                             path='/profile' title={reduxUser.tokenData.user.authorFirstName} />
                         <div
